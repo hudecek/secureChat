0d60bb594837518ffd43a5b3b66d8af42eac1580
Jan Hudecek <jhudecek17@gmail.com>
2015-11-25 19:14:33 -0600

    Almost done!

--- secureChat/index.js
+++ secureChat/index.js
@@ -17,20 +17,45 @@ http.listen(3000, function(){
 
 var nicknames = [];
 var rooms = []; //rooms by ID
+var noRooms = 1;
+
+var users = [];
+
+
 var error = 0;
+
+
 var rsa = forge.pki.rsa;
 var keypair = rsa.generateKeyPair({bits: 2048, e: 0x10001});
 
 var key = forge.random.getBytesSync(16);
 var iv = forge.random.getBytesSync(16);
 
+function room(id, name, userid, publicKey) {
+	this.id = id;
+	this.name = name;
+	this.admin = userid;
+	this.pubKey = publicKey;
+	this.users = new Array;
+	this.users.push(userid);
+}
+
+function checkCloseRooms(no, id) {
+	if(no != 0) {
+		if(no < noRooms && typeof(rooms[no]) != 'undefined') {
+			if(rooms[no].admin == id) {
+				io.sockets.emit('roomClosed', no);
+			}
+		}
+	}
+}
+
+
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    socket.on('disconnect', function() {
-    	delete nicknames[socket.id];
-  		console.log('user disconnected');
-  	});
+
+    //send all available rooms
 
     socket.on('chatStart', function(name){
 	  	for(var nick in nicknames) {
@@ -53,10 +78,86 @@ io.on('connection', function(socket){
   		keypair.publicKey.e.data = data.e.data;
   		keypair.publicKey.n.data = data.n.data;
   		socket.emit("publicChatPass", keypair.publicKey.encrypt(iv), keypair.publicKey.encrypt(key));
+  		socket.join(0);
+  		users[socket.id] = 0;
   	}); 
 
-  	socket.on('message', function(roomID, data) {
+  	socket.on('createRoom', function(name, publicKey) {
 
-  	})
+  		name.replace(/<(?:.|\n)*?>/gm, '');
+
+
+
+  		if(checkRooms(name) && name != 'Public') {
+	  		rooms[noRooms] = new room(noRooms, name, socket.id, publicKey);
+	  		socket.leave(users[socket.id]);
+	  		users[socket.id] = noRooms;
+	  		socket.join(noRooms);
+	  		io.sockets.emit('newRoom', name, noRooms);
+	  		socket.emit('acceptRoom', noRooms);
+
+	  		noRooms++;
+  		}
+  	});
+
+  	socket.on('changeRoomReq', function(id) {
+  		if(id != 0) {
+	  		if(id < noRooms && typeof(rooms[id]) != 'undefined') {
+	  			if(typeof(rooms[id]).admin != 'undefined') {
+	  				socket.emit('adminPubKey', rooms[id].pubKey);
+	  			}
+	  		}
+	  	} else {
+	  		checkCloseRooms(users[socket.id], socket.id);
+	  		socket.leave(users[socket.id]);
+	  		users[socket.id] = 0;
+	  		socket.join(0);
+	  	}
+  	});
+
+  	socket.on('roomPass', function(id, passwd, pKey) {
+  		if(id < noRooms && typeof(rooms[id]) != 'undefined') {
+	  		if(typeof(rooms[id]).admin != 'undefined') {
+  				io.to(rooms[id].admin).emit('roomConnectReq', passwd, socket.id, pKey);
+  			}
+  		}
+  	});
+
+  	socket.on('finalRoomSwap', function(id) {
+  		checkCloseRooms(users[socket.id], socket.id);
+  		socket.leave(users[socket.id]);
+  		users[socket.id] = id;
+  		socket.join(id);
+  	});
+
+  	socket.on('acceptConnectReq', function(id, encIV, encPass) {
+  		io.to(id).emit('roomAllowed', users[socket.id], encIV, encPass);
+  	});
+
+  	socket.on('message', function(data) {
+  		io.to(users[socket.id]).emit('message', nicknames[socket.id], data);
+  	});
+
+
+  	socket.on('disconnect', function() {
+		delete nicknames[socket.id];
+		if(typeof(users[socket.id]) != 'undefined')
+			if(users[socket.id] != 0) {
+					if(rooms[users[socket.id]].admin == socket.id) {
+					io.sockets.emit('roomClosed', users[socket.id]);
+					delete rooms[users[socket.id]];
+				}
+				//send socket about closing room
+			}
+		console.log('user disconnected');
+	});
 
 });
+ 
+function checkRooms(name) {
+	for(var room in rooms) {
+		if(rooms[room].name == name) 
+			return 0;
+	}
+	return 1;
+}
--- secureChat/public/client.js
+++ secureChat/public/client.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
 	$(".emptyClick").focus(function() {
 		$(this).val("");
 		$(this).css("color", "black");
@@ -9,6 +11,32 @@ $(document).ready(function() {
 	}, function() {
 		$(this).css("background-color", "#EF0000");
 	});
+
+	$("#send").click(function() {
+		send();
+	});
+
+	$('#message').keydown(function (event) {
+	    var keypressed = event.keyCode || event.which;
+	    if (keypressed == 13) {
+	        send();
+    	}
+	});
+
+	$("#newRoom").click(function() {
+		counter++;
+		
+		if(counter % 2 == 1) {
+			$("#newRoomHeading").text("Add");
+		} else {
+			$("#newRoomHeading").text("New Room");
+			socket.emit('createRoom', $("#newRoomName").val(), keypair.publicKey);
+		}
+		$("#newRoomDetails").toggle();
+	});
+
+	var counter = 0;
+
 
 	$(window).resize(function(){
 	  	setHeight();
@@ -22,6 +50,7 @@ $(document).ready(function() {
 		$("#container").html("<p>Your browser is not supported!</p>");
 	}
 
+
 	setHeight();
 });
 
@@ -31,8 +60,12 @@ var socket = io();
 
 var rsa = forge.pki.rsa;
 var keypair = rsa.generateKeyPair({bits:2048, e:0x10001});
-var iv, key;
-
+var adminKeyPair = rsa.generateKeyPair({bits:2048, e:0x10001});
+var publiv, publkey, iv, key;
+var myRoom = 0;
+var ownedRoomPass;
+var decipher, cipher;
+var requestedRoomID;
 
 function setHeight() {
 	$("#container").height($(window).innerHeight() - $("#menu").outerHeight() - $("#footer").outerHeight());
@@ -87,41 +120,147 @@ function start() {
 		socket.emit("chatStart", nick);
 	}
 }
-/*
-function exchangeKey() {
-	var iv = forge.random.getBytesSync(12);
-	var someBytes = 'hello world!';
-	var cipher = forge.cipher.createCipher('AES-GCM', result.key);
-	cipher.start({iv: iv});
-	cipher.update(forge.util.createBuffer(someBytes));
-	cipher.finish();
-	var encrypted = cipher.output.getBytes();
-	var tag = cipher.mode.tag.getBytes();
 
-	console.log(encrypted, iv, tag);
 
-	socket.emit('key', {encrypted, iv, tag});
+function changeRoom(id) {
+	if(id != myRoom) {
+		requestedRoomID = id;
+		socket.emit("changeRoomReq", id);
+		console.log(id);
+		if(id == 0) {
+			key = publkey;
+			iv = publiv;
+			updateCipher();
+			$("#" + myRoom.toString()).removeClass("active");
+			myRoom = 0;
+			$("#" + myRoom.toString()).addClass("active");
+		}
+	}
 }
-*/
+
+function send() {
+	var text = $("#message").val();
+	if(text != "" && text != "\n") { 
+		var encText = encrypt(text);
+		socket.emit('message', encText);
+		$("#message").val("");
+	}
+}
+
+function encrypt(text) {
+	cipher.start({iv: iv});
+	cipher.update(forge.util.createBuffer(text));
+	cipher.finish();
+	var encrypted = cipher.output;
+	return encrypted;
+}
+
+function decrypt(text) {
+	decipher.start({iv: iv});
+	decipher.update(text);
+	decipher.finish();
+	return decipher.output.toString();
+}
+ 
+function updateCipher() {
+	cipher = forge.cipher.createCipher('AES-CBC', key);
+	decipher = forge.cipher.createDecipher('AES-CBC', key);
+}
+
+
 socket.on("nickInfo", function(data) {
 	if(data['available']) {
 		$("#login").css("display", "none");
 		$("#container").fadeTo(0, 1.0);
 		setHeight();
 		socket.emit("pubKey", keypair.publicKey);
-		console.log(keypair.publicKey);
 	}
 	else {
 		$("#error").text("Nickname is in use. Choose different nickname.");
 	}
 });
 
-socket.on("publicChatPass", function(sentIV, sentKey) {
-	iv = keypair.privateKey.decrypt(sentIV);
-	key = keypair.privateKey.decrypt(sentKey);
+socket.on("publicChatPass", function(pIV, pKey) {
+	publiv = keypair.privateKey.decrypt(pIV);
+	iv = publiv;
+	publkey = keypair.privateKey.decrypt(pKey);
+	key = publkey;
+	updateCipher();
 });
 
 //setting the id
-socket.on('connect', function(){
+socket.on('connect', function() {
 	id = socket.io.engine.id;
+});
+
+socket.on('acceptRoom', function(id) {
+	var password = prompt("Enter new password:");
+	var digest = forge.md.sha256.create();
+	digest.update(password);
+	ownedRoomPass = digest.digest().toHex();
+	$("#" + myRoom.toString()).removeClass("active");
+	myRoom = id;
+	key = forge.random.getBytesSync(16);
+	iv = forge.random.getBytesSync(16);
+	updateCipher();
+
+	$("#" + id.toString()).addClass("active");
+});
+
+socket.on('newRoom', function(name, id) {
+	$("#roomlist").append('<div class="room" id="' + id +'" onclick=changeRoom(' + id + ')>' + name + '</div>');	
+});
+
+socket.on('adminPubKey', function(pubKey) {
+	adminKeyPair.publicKey.e.data = pubKey.e.data;
+	adminKeyPair.publicKey.n.data = pubKey.n.data;
+	var password = prompt("Enter password:");
+	var digest = forge.md.sha256.create();
+	digest.update(password);
+	password = digest.digest().toHex();
+	var encPass = adminKeyPair.publicKey.encrypt(password);
+	socket.emit('roomPass', requestedRoomID, encPass, keypair.publicKey);
+	console.log(keypair.publicKey);
+});
+
+socket.on('roomConnectReq', function(passwd, id, pKey) {
+	var pass = keypair.privateKey.decrypt(passwd);
+	console.log(pKey);
+	if(pass == ownedRoomPass) {
+		adminKeyPair.publicKey.n.data = pKey.n.data;
+		adminKeyPair.publicKey.e.data = pKey.e.data;
+		socket.emit('acceptConnectReq', id, adminKeyPair.publicKey.encrypt(iv), adminKeyPair.publicKey.encrypt(key));
+	}
+});
+
+socket.on('roomAllowed', function(id, rIV, rPass) {
+	console.log("allowed");
+	$("#" + myRoom.toString()).removeClass("active");
+	myRoom = id;
+	$("#" + id.toString()).addClass("active");
+	key = keypair.privateKey.decrypt(rPass);
+	iv = keypair.privateKey.decrypt(rIV);
+	updateCipher();
+	socket.emit('finalRoomSwap', id);
+});
+
+socket.on('message', function(name, text) {
+	var buffer = encrypt('text');
+	buffer.data = text.data;
+	buffer.read = text.read;
+	buffer._constructedStringLength = text._constructedStringLength;
+	$("#chatContains").append("<div class='received'><p class='receiverName'>" + name + ":</p>"+ decrypt(buffer) + "</div>");
+	$("#chatContains").scrollTop($("#chatContains").height());
+});
+
+socket.on('roomClosed', function(id) {
+	if(myRoom == id) {
+		myRoom = 0;
+		key = publkey;
+		iv = publiv;
+		updateCipher();
+		$("#" + myRoom.toString()).addClass("active");
+		socket.emit('finalRoomSwap', 0);
+	}
+	$("#" + id.toString()).remove();
 });
